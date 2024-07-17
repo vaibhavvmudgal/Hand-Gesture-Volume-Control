374c50656ae9fccf8fde57d494f3b7c2109b6301
Vaibhav Mudgal <86943407+vaibhavvmudgal@users.noreply.github.com>
2024-07-18 04:04:56 +0530

    Update HandModule.py

--- HandModule.py
+++ HandModule.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import mediapipe as mp
-import time
 
 class handDetector:
     def __init__(self, mode=False, maxHands=2, detCon=0.5, trackCon=0.5):
@@ -14,8 +13,8 @@ class handDetector:
         self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, video_data, draw=True):
-        #videoRGB = cv.cvtColor(video_data, cv.COLOR_BGR2RGB)
-        self.results = self.hands.process(video_data)
+        videoRGB = cv.cvtColor(video_data, cv.COLOR_BGR2RGB)  # Convert to RGB
+        self.results = self.hands.process(videoRGB)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
@@ -23,7 +22,6 @@ class handDetector:
                     self.mpDraw.draw_landmarks(video_data, handLms, self.mpHands.HAND_CONNECTIONS)
 
         return video_data
-    
 
     def findPosition(self, video_data, handNo=0, draw=True):
         lmList = []
@@ -38,35 +36,3 @@ class handDetector:
                     cv.circle(video_data, (cx, cy), 15, (255, 0, 255), cv.FILLED)
 
         return lmList
-    
-def main():
-    cap = cv.VideoCapture(0)
-    detector = handDetector()
-    cTime = 0
-    pTime = 0
-
-    while True:
-        success, video_data = cap.read()
-        if not success:
-            break
-
-        video_data = detector.findHands(video_data)
-        lmList = detector.findPosition(video_data)
-        if lmList:
-            print(lmList[4])
-        
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv.putText(video_data, str(int(fps)), (10, 70), cv.FONT_HERSHEY_DUPLEX, 3, (0, 255, 255), 4)
-        cv.imshow("LIVE", video_data)
-
-        if cv.waitKey(10) == ord("a"):
-            break
-
-    cap.release()
-    cv.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
